35207844033d89e2b866c91834901bf3cf6717a0
sameheddine <sameheddine.hchaichi@gmail.com>
2021-06-27 23:28:51 +0100

    comit1

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,75 @@
-#include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+
+#include <NTPClient.h>
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include <TM1637Display.h>     //
+
+#define CLK 22                     // Define the connections pins:
+#define DIO 23
+
+TM1637Display display = TM1637Display(CLK, DIO);              // Create display object of type TM1637Display:
+
+const char *ssid     = "TOPNET_Karim_Ext";
+const char *password = "ksmk@050703";
+#define WIFI_TIMEOUT_MS 2000
+
+const long utcOffsetInSeconds = 3600;  //Tunisia time zone is GMT+1 = 1*60*60 = 3600seconds difference
+
+
+// Define NTP Client to get time
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
+
+uint32_t chipId = 0;
+
+
+
+void setup(){ 
+  Serial.begin(115200);
+   // Clear the display:
+  display.clear();
+  
+  WiFi.begin(ssid, password);
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println(".");
+    Serial.print("Conntect to ");
+    Serial.println(ssid);
+    Serial.println("ESP IP");
+    Serial.println(WiFi.localIP());
+    Serial.println("MAC Address");
+    Serial.println(WiFi.macAddress());
+    Serial.println(timeClient.getHours());
+    Serial.println(timeClient.getMinutes());
+
+  }
+  
+  
+  
+  timeClient.begin();
+  
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int A,B;
+  
+  timeClient.update();
+  display.setBrightness(7);                   // Set the brightness:
+  
+  A = timeClient.getHours() * 100 + timeClient.getMinutes();
+  B = timeClient.getSeconds();
+  
+  if((B % 2) == 0)
+  {
+    display.showNumberDecEx(A, 0b01000000 , false, 4, 0); 
+  }
+  else
+  {
+    display.showNumberDecEx(A, 0b00000000 , false, 4, 0); 
+  }
+ 
 }
