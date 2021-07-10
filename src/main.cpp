027906c27e823532e2e79e20bc64dc628bc32e49
sameheddine <sameheddine.hchaichi@gmail.com>
2021-07-10 14:57:12 +0100

    v1

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 // Define the connections pins to TM1637Display:
 #define CLK 22                     
 #define DIO 23
-// Create display object of type TM1637Display:
+// Create display object of type TM1637Display
 TM1637Display display = TM1637Display(CLK, DIO);              
 
 const char *ssid     = "TOPNET_Karim_Ext";
