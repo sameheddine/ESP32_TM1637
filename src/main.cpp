2c09e14b168f9bab5e5e6264885adcde92c5dcbc
sameheddine <sameheddine.hchaichi@gmail.com>
2021-07-10 15:23:00 +0100

    v1

--- src/main.cpp
+++ src/main.cpp
@@ -10,54 +10,58 @@
 // Create display object of type TM1637Display:
 TM1637Display display = TM1637Display(CLK, DIO);              
 
+// Network credentials
 const char *ssid     = "TOPNET_Karim_Ext";
 const char *password = "ksmk@050703";
 
 
-const long utcOffsetInSeconds = 3600;  //Tunisia time zone is GMT+1 = 1*60*60 = 3600seconds difference
 
 
 // Define NTP Client to get time
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
-
-uint32_t chipId = 0;
+NTPClient timeClient(ntpUDP);
 
 
 
-void setup(){ 
+
+void setup(){
+  // Initialize Serial Monitor
   Serial.begin(115200);
-   // Clear the display:
-  display.clear();
-  
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
   WiFi.begin(ssid, password);
-
-  // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.println(".");
-    Serial.print("Conntect to ");
-    Serial.println(ssid);
-    Serial.println("ESP IP");
-    Serial.println(WiFi.localIP());
-    Serial.println("MAC Address");
-    Serial.println(WiFi.macAddress());
-    Serial.println(timeClient.getHours());
-    Serial.println(timeClient.getMinutes());
-
+    Serial.print(".");
   }
-  
-  
-  
+
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.print("Conntect to ");
+  Serial.println(ssid);
+  Serial.println("ESP IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println("MAC Address");
+  Serial.println(WiFi.macAddress());
+  // Initialize a NTPClient to get time
   timeClient.begin();
-  
-  
+  // Set offset time in seconds to adjust for your timezone, for example:
+  //Tunisia time zone is GMT+1 = 1*60*60 = 3600seconds difference
+  // GMT +1 = 3600
+  // GMT +8 = 28800
+  // GMT -1 = -3600
+  // GMT 0 = 0
+  timeClient.setTimeOffset(3600);
+  display.clear();
 }
 
 void loop() {
+  while(!timeClient.update()) {
+    timeClient.forceUpdate();
+  }
   int A,B;
-  
-  timeClient.update();
+    
   display.setBrightness(7);                   // Set the brightness:
   
   A = timeClient.getHours() * 100 + timeClient.getMinutes();
